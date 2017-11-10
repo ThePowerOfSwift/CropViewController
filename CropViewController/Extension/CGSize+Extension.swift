20dbbb81177b51701e050cf3bcc64a467ffa06ff
ha1f <ha1f@linecorp.com>
2017-11-10 20:30:37 +0900

    Add comments

--- CropViewController/Extension/CGSize+Extension.swift
+++ CropViewController/Extension/CGSize+Extension.swift
@@ -9,21 +9,41 @@
 import UIKit
 
 extension CGSize {
-    func scaled(by scale: CGFloat) -> CGSize {
+    /// Calculate the uniformly scaled size.
+    ///
+    /// - parameter scale: The scale to apply
+    ///
+    /// - returns: The calculated size
+    func uniformlyScaled(by scale: CGFloat) -> CGSize {
         return CGSize(width: width * scale, height: height * scale)
     }
     
+    /// Calculate the x/y scale to fit to another size.
+    ///
+    /// - parameter otherSize: The size which you want to fill.
+    ///
+    /// - returns: The calculated scale tuple with format (widthScale, heightScale)
     private func _scaleFitScales(to otherSize: CGSize) -> (widthScale: CGFloat, heightScale: CGFloat) {
         let widthScale = self.width != 0 ? (otherSize.width / self.width) : 0
         let heightScale = self.height != 0 ? (otherSize.height / self.height) : 0
         return (widthScale: widthScale, heightScale: heightScale)
     }
     
+    /// Calculate the scale to fill another size with keeping own aspect ratio.
+    ///
+    /// - parameter otherSize: The size which you want to fill.
+    ///
+    /// - returns: The calculated scale
     func aspectFillScale(to otherSize: CGSize) -> CGFloat {
         let scales = _scaleFitScales(to: otherSize)
         return max(scales.widthScale, scales.heightScale)
     }
     
+    /// Calculate the scale to fit to another size with keeping own aspect ratio.
+    ///
+    /// - parameter otherSize: The size to which you want to fit.
+    ///
+    /// - returns: The calculated scale
     func aspectFitScale(to otherSize: CGSize) -> CGFloat {
         let scales = _scaleFitScales(to: otherSize)
         return min(scales.widthScale, scales.heightScale)
